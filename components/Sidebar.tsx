b103ddb95e797bbf92b5d1e065bece7aa975fc9b
nodirbeksulaymonov <s.nodir9494@gmail.com>
2024-07-29 02:18:54 +0500

    add sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import React from 'react'
+
+const Sidebar = ({ user }: SiderbarProps) => {
+  return (
+    <section className='sidebar'>
+        <nav className='flex flex-col gap-4'>
+            <Link href='/' className='mb-12 cursor-pointer items-center gap-2'>
+                <Image
+                  src='/icons/logo.svg'
+                  width={34}
+                  height={34}
+                  alt='Horizon logo'
+                  className='size-[24px] max-xl:size-14'
+                />
+            </Link>
+        </nav>
+    </section>
+  )
+}
+
+export default Sidebar
