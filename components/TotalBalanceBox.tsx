e7ceb854403bed3e9982f8f11e056f18cedfdb4d
nodirbeksulaymonov <s.nodir9494@gmail.com>
2024-07-28 20:13:33 +0500

    feat: implement chartJS

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -1,4 +1,7 @@
 import { formatAmount } from '@/lib/utils';
+import CountUp from 'react-countup';
+import AnimatedCounter from './AnimatedCounter';
+import DoughnutChart from './DoughnutChart';
 
 const TotalBalanceBox = ({
     accounts = [], totalBanks, totalCurrentBalance
@@ -6,7 +9,7 @@ const TotalBalanceBox = ({
   return (
     <section className='total-balance'>
         <div className='total-balance-chart'>
-            {/* DoughnutChart*/}
+            <DoughnutChart accounts={accounts}/>
         </div>
 
         <div className='flex flex-col gap-6'>
@@ -18,9 +21,9 @@ const TotalBalanceBox = ({
                     Total Current Balance
                 </p>
 
-                <p className='total-balance-amount flex-center gap-2'>
-                    {formatAmount(totalCurrentBalance)}
-                </p>
+                <div className='total-balance-amount flex-center gap-2'>
+                    <AnimatedCounter amount={totalCurrentBalance} />
+                </div>
         </div>
         </div>
     </section>
